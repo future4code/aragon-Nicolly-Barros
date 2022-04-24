998630f0ed44693c42ca7781f9ebca5f7fd16956
Nicoly Barros <nicolybarros00@gmail.com>
2022-04-24 15:55:14 -0300

    Feito parte obrigatoria do projeto, verificando desafios.

--- modulo2/projeto-whatslab/whats-lab/src/App.js
+++ modulo2/projeto-whatslab/whats-lab/src/App.js
@@ -1,76 +1,39 @@
-import './App.css';
 import React from 'react';
+import styled from 'styled-components';
+import EnviarMensagem from './components/EnviarMensagem';
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 600px;
+  height: 100vh;
+  border: 1px solid black;
+  flex: 1;
+  /* max-width: 600px;
+  height: 100vh;
+  border: 1px solid black;
+  flex: 1;
+  display: flex;
+  flex-direction: column; */
+`
+
+const ContainerMensagens = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+`
+
 
 class App extends React.Component {
-
-  state ={
-    mensagens: [
-      {
-        nome: "oi",
-        mensagem: "",
-      }
-    ],
-    valorInputNome: "",
-    valorInputMensagem: ""
-  }
-
-  enviarMensagem = () => {
-    const novaMensagem = {
-
-      nome: this.state.valorInputNome,
-      mensagem: this.state.valorInputFoto,
-    }
-
-    const novasMensagens = [...this.state.postagens, novaMensagem];
-    this.setState({ mensagens: novasMensagens});
-    this.setState({valorInputNome: ""})
-    this.setState({valorInputMensagem: ""})
-  }
-  
-  onChangeInputNome = (event) => {
-    this.setState({ valorInputNome: event.target.value });
-  };
-
-  onChangeInputMensagem = (event) => {
-    this.setState({ valorInputMensagem: event.target.value });
-  };
-
   render(){
-    const verMensagens = this.state.mensagens.map((mensagem) => {
-      return(
-        <Post
-          nome= {mensagem.nome}
-          mensagem= {mensagem.mensagem}
-        />
-      );
-    });
     return(
-      <div>
-          <div>
-            <h2>Faça uma nova postagem</h2>
-            <Inputs
-              value={this.state.valorInputNome}
-              onChange={this.onChangeInputNome}
-              placeholder={"Nome"}
-            />
-
-            <Inputs
-              value={this.state.valorInputFoto}
-              onChange={this.onChangeInputFoto}
-              placeholder={"Foto de Usuário (link/URL)"}
-            />
-
-            <Inputs 
-              value={this.state.valorInputPostagem}
-              onChange={this.onChangeInputPostagegm}
-              placeholder={"Foto do Post (link/URL)"}
-            />
-
-            <button onClick={this.adicionarPostagem}>Postar</button>
-          </div>
-
-          <div>{verMensagens}</div>
-      </div>
+      <Main>
+          <ContainerMensagens>
+            <EnviarMensagem/>
+          </ContainerMensagens>
+      </Main>
     )
   }
 }
