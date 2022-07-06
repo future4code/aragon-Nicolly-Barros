5cc01675e11da1053b556ff089e6d1c2bd31434f
Nicoly Barros <nicolybarros00@gmail.com>
2022-07-06 15:56:36 -0300

    Feito exercicios da aula fluxos de dados

---
+++ modulo4/fluxo-de-dados-backend/fluxo-de-dados-atividade/src/data.ts
@@ -0,0 +1,23 @@
+export type Produto = {
+    id: string,
+    name: string,
+    price: number
+}
+
+export let produtos: Produto[] = [
+    {
+        id: "1",
+        name: "bolsa xadrez",
+        price: 29.80
+    },
+    {
+        id: "2",
+        name: "bolsa onça",
+        price: 45.90
+    },
+    {
+        id: "3",
+        name: "bolsa camurça",
+        price: 57.99
+    },
+]
